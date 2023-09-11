8c4159c986415f1c220934fc57d5e65ffdf1e570
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-11 16:38:47 +0300

    create validation middleware

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,12 +1,21 @@
-const { body, validatinResult } = require("express-validator");
+const { body, param, validationResult } = require("express-validator");
 
 const validateAddPerson = [
-	body("name").notEmpty().isString().withMessage("Name must be a string"),
+	body("name")
+		.notEmpty()
+		.isString()
+		.escape()
+		.withMessage("Name must be a valid string"),
 	body("email").notEmpty().isEmail().withMessage("Email must be a valid email"),
-	body("country").notEmpty().isString().withMessage("Country must be a string"),
+	body("age").notEmpty().isInt().withMessage("Age must be an integer"),
+	body("country")
+		.notEmpty()
+		.isString()
+		.escape()
+		.withMessage("Country must be a valid string"),
 
 	(req, res, next) => {
-		const errors = validatinResult(req);
+		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
@@ -15,3 +24,59 @@ const validateAddPerson = [
 		next();
 	},
 ];
+
+const validateGetPerson = [
+	param("id").notEmpty().isInt().withMessage("ID must be an integer"),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		next();
+	},
+];
+
+const validateUpdatePerson = [
+	param("id").notEmpty().isInt().withMessage("ID must be an integer"),
+	body("name")
+		.isString()
+		.escape()
+		.optional()
+		.withMessage("Name must be a valid string"),
+	body("email").isEmail().optional().withMessage("Email must be a valid email"),
+	body("age").isInt().optional().withMessage("Age must be an integer"),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		next();
+	},
+];
+
+const validateDeletePerson = [
+	param("id").notEmpty().isInt().withMessage("ID must be an integer"),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		next();
+	},
+];
+
+module.exports = {
+	validateAddPerson,
+	validateDeletePerson,
+	validateGetPerson,
+	validateUpdatePerson,
+};
