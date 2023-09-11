3a2f02a70074691d3a28479f3a662cf3b60bd8ed
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-11 13:24:01 +0300

    use environment variable port if available

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", personRoute);
 
-app.listen(3000, () => {
-	console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
