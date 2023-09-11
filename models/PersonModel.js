79f8a2f85040f0d241c641f70b8570c4224c025e
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-11 16:38:22 +0300

    rename Person to PersonModel

---
+++ models/PersonModel.js
@@ -0,0 +1,74 @@
+const supabase = require("../utils/supabase");
+
+class PersonModel {
+	/**
+	 * Add a new person to the database
+	 */
+	static async add(person) {
+		const { data, error } = await supabase
+			.from("person")
+			.insert(person)
+			.select()
+			.single();
+
+		if (error) {
+			return { error };
+		}
+
+		return { code: 201, message: "Person is added to the database", data };
+	}
+
+	/**
+	 * Get a person from the database
+	 */
+	static async get(id) {
+		const { data, error } = await supabase
+			.from("person")
+			.select("*")
+			.eq("id", id)
+			.single();
+
+		if (error) {
+			return { error };
+		}
+
+		return {
+			code: 200,
+			message: "Person is retrieved from the database",
+			data,
+		};
+	}
+
+	/**
+	 * Update a person in the database
+	 */
+	static async update(id, person) {
+		const { data, error } = await supabase
+			.from("person")
+			.update(person)
+			.eq("id", id)
+			.select()
+			.single();
+
+		if (error) {
+			return { error };
+		}
+
+		return { code: 200, message: "Person is updated in the database", data };
+	}
+
+	/**
+	 * Delete a person from the database
+	 */
+	static async delete(id) {
+		const { error } = await supabase.from("person").delete().eq("id", id);
+
+		if (error) {
+			return { error };
+		}
+
+		return { code: 200, message: "Person is deleted from the database" };
+	}
+}
+
+module.exports = PersonModel;
